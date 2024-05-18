0d8d834a6f1786583fca5aaf6db344a65699e8d7
hrttf111 <33761444+hrttf111@users.noreply.github.com>
2024-05-18 18:10:44 +0300

    Changed water generation script

--- scripts/make_water.sh
+++ scripts/make_water.sh
@@ -1,6 +1,8 @@
-rm -rf ./img
+EXEC="cargo run --bin pop_res --release -- "
+
+#rm -rf ./img
 mkdir ./img
 
 for i in `seq 0 255`; do
-    cargo run --bin pop_res --release -- water 1 $i > img/water_${i}.bmp
+    ${EXEC} water 1 $i > img/water_${i}.bmp
 done
